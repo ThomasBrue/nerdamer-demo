8bd46bdeea0508f5df0bb6aa3a71dfab3007e623
ThomasB <t.bruendl@gmail.com>
2021-03-30 21:39:42 +0200

    first commit nerdamer-demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import * as nerdamer from 'nerdamer/all';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'nerdamer-demo';
+
+  constructor() {
+    //--Evaluation----evaluate()------------------------------------------------------------------------------------
+    console.log(nerdamer('x^2', { x: '2' }).evaluate().text('fractions'));
+    //--Differentiation----diff()------------------------------------------------------------------------------------
+    console.log(nerdamer('diff(x^2,x)').text('fractions'));
+    //--integration----integrate()------------------------------------------------------------------------------------
+    console.log(nerdamer('integrate(x^2,x)').text('fractions'));
+  }
 }
